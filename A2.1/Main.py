d614a7beeb48d624db58d2eca2f44b5964422a3f
ap17059 <m.z.simjee@se17@qmul.ac.uk>
2017-10-17 11:10:47 +0100

    Exercise A2.1 finished

--- A2.1/Main.py
+++ A2.1/Main.py
@@ -4,7 +4,13 @@ plt.axis([0,500,0,500])
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('A2.1 Square')
-x= [250]
-y= [250]
-plt.plot(x,y,'+k')
+a= [250]
+b= [250]
+c= [100,400,400,100,100]
+d= [100,100,400,400,100]
+x= [50,450,450,50,50]
+y= [50,50,450,450,50]
+plt.plot(a,b,'+k')
+plt.plot(c,d, 'r')
+plt.plot(x,y, 'b')
 plt.show()
