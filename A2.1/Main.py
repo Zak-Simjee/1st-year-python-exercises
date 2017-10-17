a04f8e18c3fe36c27edf2ca32e4124dbe539373b
ap17059 <m.z.simjee@se17@qmul.ac.uk>
2017-10-17 11:01:04 +0100

    Exercise A2.1

---
+++ A2.1/Main.py
@@ -0,0 +1,10 @@
+from matplotlib import pyplot as plt
+plt.axis('square')
+plt.axis([0,500,0,500])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('A2.1 Square')
+x= [250]
+y= [250]
+plt.plot(x,y,'+k')
+plt.show()
