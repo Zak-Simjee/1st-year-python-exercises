b0f3f5a0f61e3a0540976d3164935d8a8b0ff321
ap17059 <m.z.simjee@se17@qmul.ac.uk>
2017-10-23 16:31:38 +0100

    Exercise A2.2 finished

--- A2.2/Main.py
+++ A2.2/Main.py
@@ -2,10 +2,10 @@ from matplotlib import pyplot as plt
 
 length = float(input())
 if length<0:
-    print('too low')
+    print('Error: Value too low')
     exit(1)
 if length>500:
-    print('too high')
+    print('Error: Value too high')
     exit(1)
 plt.axis('square')
 plt.axis([0,500,0,500])
@@ -13,4 +13,7 @@ plt.title('A2.2')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.plot(250,250, 'k+')
+x = [250-(length/2),250+(length/2),250+(length/2),250-(length/2),250-(length/2)]
+y = [250-(length/2),250-(length/2),250+(length/2),250+(length/2),250-(length/2)]
+plt.plot(x,y, 'b')
 plt.show()
