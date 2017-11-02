7f9b67ac8c26023bed9794bcacedb992b56f18fb
ap17059 <m.z.simjee@se17@qmul.ac.uk>
2017-11-02 13:10:57 +0000

    Exercise A2.5 finished

--- A2.5/Main.py
+++ A2.5/Main.py
@@ -7,24 +7,32 @@ def polygon(nsides, length, xc, yc, angle):
     for i in range(nsides+1):
         x = xc + r*math.sin((2*math.pi*i/nsides)+ angle)
         y = yc + r*math.cos((2*math.pi*i/nsides)+ angle)
-    coordinate = (x, y)
-    coordinates.append(coordinate)
+        coordinate = (x, y)
+        coordinates.append(coordinate)
     return coordinates
 
-nsides = int(input('Enter number of side'))
+nsides = int(input('Enter number of sides: '))
 if nsides<2:
-    print('Number of sides is too low')
+    print('Error: Number of sides must be greater than 2')
     exit(1)
 
-length = float(input('Enter length'))
+length = float(input('Enter length: '))
 if length<0:
-    print('Length is too small')
+    print('Error: Length cannot be less than 0')
     exit(1)
 
-xc = float(input('Enter centre x point'))
-yc = float(input('Enter centre y point'))
-angle = float(input('Enter angle'))
+xc = float(input('Enter centre x point: '))
+yc = float(input('Enter centre y point: '))
+angle = float(input('Enter angle: '))
 calculatedcoordinates = polygon(nsides, length, xc, yc, angle)
 fr = open('points.txt', 'w')
-fr.write(len(calculatedcoordinates))
-fr.write(calculatedcoordinates)
+fr.write(str(len(calculatedcoordinates)))
+fr.write('\n')
+for a in range(len(calculatedcoordinates)):
+    xcoord = calculatedcoordinates[a][0]
+    ycoord = calculatedcoordinates[a][1]
+    coords = '{0:.2f},{1:.2f}'
+    finalcoords = coords.format(xcoord, ycoord)
+    fr.write(finalcoords)
+    fr.write('\n')
+fr.close()
